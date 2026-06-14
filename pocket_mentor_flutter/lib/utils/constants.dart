6ec4d1eed64c5f05866039ddc6d862d0711b504d
GuptaDeepak9264 <150012848+GuptaDeepak9264@users.noreply.github.com>
2026-06-15 00:09:30 +0530

    Update base URL for API endpoint

--- pocket_mentor_flutter/lib/utils/constants.dart
+++ pocket_mentor_flutter/lib/utils/constants.dart
@@ -1,6 +1,7 @@
 class AppConstants {
   // API
-  static const String baseUrl = 'http://10.0.2.2:8000/api/v1'; // Android emulator localhost
+  // static const String baseUrl = 'http://10.0.2.2:8000/api/v1'; // Android emulator localhost
+  static const String baseUrl = 'https://pocket-mentor.onrender.com/api/v1';
   static const int connectTimeoutMs = 10000;
   static const int receiveTimeoutMs = 30000;
 
